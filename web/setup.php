df96ccaa41ac7bf30f3a595eb60d4865133e0740
Andy Chase <asperous2@gmail.com>
2015-08-05 07:17:59 -0700

    Api/setup should check array first

--- web/setup.php
+++ web/setup.php
@@ -5,7 +5,7 @@ require('../vendor/autoload.php');
 $max_data_length = 20010;
 // ^ Calculated like so: len("789cae03b53fe955bc6746e7ee0663" + (":o331HlIeYrw:999:999"*999))
 
-if (!$_ENV['REDIS_URL'])
+if (!array_key_exists('REDIS_URL', $_ENV))
     $_ENV['REDIS_URL'] = "redis://h:@localhost:6379";
 
 $redis = new Predis\Client([
