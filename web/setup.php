3dfe4781447560cec3ee1c9ea76d556f062e6adc
Andy Chase <asperous2@gmail.com>
2015-08-06 20:39:00 -0700

    Fix redis environment variable overwriting
    On production environments, this command seems to fail

--- web/setup.php
+++ web/setup.php
@@ -5,11 +5,12 @@ require('../vendor/autoload.php');
 $max_data_length = 20010;
 // ^ Calculated like so: len("789cae03b53fe955bc6746e7ee0663" + (":o331HlIeYrw:999:999"*999))
 
-if (!array_key_exists('REDIS_URL', $_ENV))
-    $_ENV['REDIS_URL'] = "redis://h:@localhost:6379";
+$redis_url = $_ENV['REDIS_URL'];
+if (!isset($redis_url))
+    $redis_url = "redis://h:@localhost:6379";
 
 $redis = new Predis\Client([
-    'host' => parse_url($_ENV['REDIS_URL'], PHP_URL_HOST),
-    'port' => parse_url($_ENV['REDIS_URL'], PHP_URL_PORT),
-    'password' => parse_url($_ENV['REDIS_URL'], PHP_URL_PASS),
+    'host' => parse_url($redis_url, PHP_URL_HOST),
+    'port' => parse_url($redis_url, PHP_URL_PORT),
+    'password' => parse_url($redis_url, PHP_URL_PASS),
 ]);
