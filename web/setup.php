d157cf4976ae15538eca4277b74e7f81b91275d1
Andy Chase <asperous2@gmail.com>
2015-08-06 21:37:34 -0700

    Changes to redis url (again) for development

--- web/setup.php
+++ web/setup.php
@@ -5,8 +5,8 @@ require('../vendor/autoload.php');
 $max_data_length = 20010;
 // ^ Calculated like so: len("789cae03b53fe955bc6746e7ee0663" + (":o331HlIeYrw:999:999"*999))
 
-$redis_url = $_ENV['REDIS_URL'];
-if (!isset($redis_url))
+$redis_url = getenv("REDIS_URL");
+if (!isset($redis_url) || !$redis_url)
     $redis_url = "redis://h:@localhost:6379";
 
 $redis = new Predis\Client([
