afe6849ef63fca25d259a8c626a8c3590887bb3c
Andy Chase <asperous2@gmail.com>
2015-08-07 22:25:57 -0700

    Add google tracking

--- web/watch.php
+++ web/watch.php
@@ -125,5 +125,15 @@ if ($name == "")
          data-ad-slot="1417591750"></ins>
 </div>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-58719971-2', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
